daeb24a2b61854e9282c30f0fd308d3d4e2795eb
darkone0112 <darkone0112@gmail.com>
2023-01-20 21:30:18 +0100

    CosladaAndPozueloTablesAddedPendingInventaryClass

--- Inventary/src/Inventary.java
+++ Inventary/src/Inventary.java
@@ -1,3 +1,28 @@
+
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 public class Inventary {
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    public void loadJDBC() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error loading JDBC driver: " + e);
+        }
+    }
+
+    public void conectar() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventario", "VsCode", "2458");
+            System.out.println("Connection established successfully.");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database: " + e);
+        }
+    }
     
 }
